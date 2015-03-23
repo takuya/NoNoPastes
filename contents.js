b7f5b6d3905ae36dd11db15cb33e21361c8da0d4
takuya <takuya.1st+nospam@gmail.com>
2015-03-23 15:31:41 +0900

    作りなおした

--- contents.js
+++ contents.js
@@ -1,3 +1,21 @@
-["onpaste","oncopy","oncut"].forEach(function(e_name){
-  Array.apply(null, document.querySelectorAll("*["+e_name+"]") ).forEach(function(e){e[e_name]="return true;"})  
-})
+document.body.appendChild(function(){
+  sc = document.createElement("script");
+  var code = function(){
+		["onpaste","oncopy","oncut"].forEach(function(e_name){
+		  Array.apply(null, document.querySelectorAll("*["+e_name+"]") ).forEach(function(e){e[e_name]="return true;"})  
+		})
+	Array.apply(null,document.querySelectorAll("form,input,textarea ")).forEach(function(e){
+		if(e.onpaste){e.onpaste=null}
+		if(e.oncopy){e.oncopy=null}
+		if(e.oncut){e.oncut=null}
+	})
+  }
+  src = "("+code.toString()+")()"
+  sc.type="text/javascript";
+  sc.text=src
+  return sc;
+}()
+);
+
+
+
